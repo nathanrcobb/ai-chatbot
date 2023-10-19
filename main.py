d1c7fac62e305345d0dffce5e23f7dcd4caedc56
Nathan Cobb <nathanrcobb@gmail.com>
2023-10-19 00:37:12 -0700

    Chatbot API working

--- main.py
+++ main.py
@@ -1,82 +1,40 @@
+import os
+
+from dotenv import load_dotenv
 import openai
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form
 from typing import Annotated
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-from mangum import Mangum
 
+# Load environment variables
+load_dotenv()
+access_token = os.getenv("OPENAI_API_KEY", "")
+if not access_token:
+    raise KeyError("OPENAI_API_KEY not found in environment.")
+openai.api_key = access_token
+
+# FastAPI app
 app = FastAPI()
-handler = Mangum(app)
 
-templates = Jinja2Templates(directory="templates")
-
-chat_responses = []
+# Log of all user, system, and assistant prompts
+chat_log = [{"role": "system", "content": "You are a helpful assistant"}]
 
 
-@app.get("/", response_class=HTMLResponse)
-async def chat_page(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request, "chat_responses": chat_responses})
+@app.post("/")
+async def chat(user_input: Annotated[str, Form()]):
+    # Append user input to chat log
+    chat_log.append({"role": "user", "content": user_input})
 
-
-chat_log = [{'role': 'system',
-             'content': 'You are a Python tutor AI, completely dedicated to teach users how to learn \
-                        Python from scratch. Please provide clear instructions on Python concepts, \
-                        best practices and syntax. Help create a path of learning for users to be able \
-                        to create real life, production ready python applications.'
-             }]
-
-
-@app.post("/", response_class=HTMLResponse)
-async def chat(request: Request, user_input: Annotated[str, Form()]):
-
-    chat_log.append({'role': 'user', 'content': user_input})
-    chat_responses.append(user_input)
-
+    # Make call to OpenAI API
     response = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
+        model="gpt-3.5-turbo",
         messages=chat_log,
-        temperature=0.6
+        temperature=0.8,
     )
 
-    bot_response = response['choices'][0]['message']['content']
-    chat_log.append({'role': 'assistant', 'content': bot_response})
-    chat_responses.append(bot_response)
+    # Get response from OpenAI API
+    bot_response = response.get("choices", [])[0].get("message").get("content")
 
-    return templates.TemplateResponse("home.html", {"request": request, "chat_responses": chat_responses})
+    # Append assistant response to chat log
+    chat_log.append({"role": "assistant", "content": bot_response})
 
-
-@app.get("/image", response_class=HTMLResponse)
-async def image_page(request: Request):
-    return templates.TemplateResponse("image.html", {"request": request})
-
-
-@app.post("/image", response_class=HTMLResponse)
-async def create_image(request: Request, user_input: Annotated[str, Form()]):
-
-    response = openai.Image.create(
-        prompt=user_input,
-        n=1,
-        size="512x512"
-    )
-
-    image_url = response['data'][0]['url']
-    return templates.TemplateResponse("image.html", {"request": request, "image_url": image_url})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return bot_response
