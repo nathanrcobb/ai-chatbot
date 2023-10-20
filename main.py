7c6339ca57fa17fc1c093e06d77b3d2475edca2d
Nathan Cobb <nathanrcobb@gmail.com>
2023-10-19 20:36:26 -0700

    Finished v1 of styling

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 
 from dotenv import load_dotenv
 import openai
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 
 # Load environment variables
@@ -14,13 +17,33 @@ openai.api_key = access_token
 
 # FastAPI app
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Log of all user, system, and assistant prompts
 chat_log = [{"role": "system", "content": "You are a helpful assistant"}]
 
+# Temporary, for testing
+chat_log.append(
+    {"role": "user", "content": "What was the most popular Christmas gift in 1989?"}
+)
+chat_log.append(
+    {
+        "role": "assistant",
+        "content": 'In 1989, one of the most popular and highly sought-after toys for Christmas was the "Game Boy" by Nintendo. The Game Boy was a handheld gaming console that allowed players to enjoy video games on the go. It came with games like "Tetris," which quickly became a hit and contributed to the Game Boy\'s immense popularity during the holiday season in 1989.',
+    }
+)
 
-@app.post("/")
-async def chat(user_input: Annotated[str, Form()]):
+
+@app.get("/", response_class=HTMLResponse)
+async def chat_page(request: Request):
+    return templates.TemplateResponse(
+        "home.html", {"request": request, "chat_log": chat_log}
+    )
+
+
+@app.post("/", response_class=HTMLResponse)
+async def chat(request: Request, user_input: Annotated[str, Form()]):
     # Append user input to chat log
     chat_log.append({"role": "user", "content": user_input})
 
@@ -37,4 +60,6 @@ async def chat(user_input: Annotated[str, Form()]):
     # Append assistant response to chat log
     chat_log.append({"role": "assistant", "content": bot_response})
 
-    return bot_response
+    return templates.TemplateResponse(
+        "home.html", {"request": request, "chat_log": chat_log}
+    )
